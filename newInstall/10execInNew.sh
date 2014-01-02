3014ce17d7255c60bf08e3d1f5ba2957c2e4674f
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-01-02 10:59:23 +0800

    modified:   newInstall/10execInNew.sh

--- newInstall/10execInNew.sh
+++ newInstall/10execInNew.sh
@@ -4,3 +4,10 @@ echo "$(whoami) ALL=(ALL:ALL) NOPASSWD:ALL" > /tmp/tmp.txt
 sudo bash -c "cat /tmp/tmp.txt >> /etc/sudoers"
 
 sudo apt-get install ssh
+
+sudo add-apt-repository ppa:tsbarnes/indicator-keylock
+sudo apt-get update
+sudo apt-get install indicator-keylock
+setsid indicator-keylock 
+
+sudo apt-get install vim-gnome 
