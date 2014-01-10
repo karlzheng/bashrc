643e6624fb1d01a97b91cd7c5bfd65fa23ecf996
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-01-10 16:42:33 +0800

    modified:   newInstall/40configInNew.sh

--- newInstall/40configInNew.sh
+++ newInstall/40configInNew.sh
@@ -11,36 +11,7 @@ sudo mkdir -p /home/share
 sudo chmod 777 /home/share
 sudo chown $(whoami).$(whoami) /home/share
 
-cat << EEOOFF > /tmp/tmp.conf
-    security = user
-[personal]
-    comment = personalFolder
-    path = /home/personal
-    public = yes
-    writable = yes
-    create mask = 0700
-    directory mask = 0700
-    ;force user = nobody
-    available = yes
-    browseable = yes
-    display charset = UTF-8
-    unix charset = UTF-8
-    dos charset = cp936
-EEOOFF
-echo "    valid users = $(whoami)" >> /tmp/tmp.conf
-echo "    force user = $(whoami)" >> /tmp/tmp.conf
-echo "    force group = $(whoami)" >> /tmp/tmp.conf
-
-cat << EEOOFF >> /tmp/tmp.conf
-[share]
-    comment = publicShare
-    path = /home/share
-    read only = no
-    public = yes
-    guest ok = yes
-EEOOFF
-echo "    force user = $(whoami)" >> /tmp/tmp.conf
-echo "    force group = $(whoami)" >> /tmp/tmp.conf
+sed -e "s#karlzheng#$(whoami)#g" samba/smb.conf > /tmp/tmp.conf
 
 sudo bash -c "cat /tmp/tmp.conf >> /etc/samba/smb.conf"
 
