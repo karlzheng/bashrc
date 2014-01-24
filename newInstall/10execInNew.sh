765152722861c3a12ff7af155f4c5c690d46f8d1
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-01-24 17:04:01 +0800

    modified:   newInstall/alreadySyncFile.list

--- newInstall/10execInNew.sh
+++ newInstall/10execInNew.sh
@@ -3,11 +3,9 @@
 echo "$(whoami) ALL=(ALL:ALL) NOPASSWD:ALL" > /tmp/tmp.txt
 sudo bash -c "cat /tmp/tmp.txt >> /etc/sudoers"
 
-sudo apt-get install ssh
+sudo apt-get install ssh -y
 
 sudo add-apt-repository ppa:tsbarnes/indicator-keylock
 sudo apt-get update
-sudo apt-get install indicator-keylock
+sudo apt-get install indicator-keylock -y
 setsid indicator-keylock 
-
-sudo apt-get install vim-gnome 
