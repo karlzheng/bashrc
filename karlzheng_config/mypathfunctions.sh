ffce0950160be0e3d30e33b51d52caba13f49749
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-03-27 10:18:26 +0800

    modified: ap func.

--- karlzheng_config/mypathfunctions.sh
+++ karlzheng_config/mypathfunctions.sh
@@ -64,6 +64,8 @@ function ap()
 		if [ $1 == '.' ];then
 			export PATH="$(pwd):$PATH"
 			echo $PATH
+			export LD_LIBRARY_PATH="$(pwd):${LD_LIBRARY_PATH}"
+			echo $LD_LIBRARY_PATH
 		fi
 	else
 		pwd | sed -e "s#^${HOME}#~#"
