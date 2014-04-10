5942860c8cb0b25b069e55d2f9ee4020bc2afbff
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-04-10 17:26:32 +0800

    new file:   script/adb.sh
    Signed-off-by: kangliang.zkl <kangliang.zkl@alibaba-inc.com>

---
+++ script/adb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  adb.sh
+# 
+#         USAGE:  ./adb.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Karl Zheng (), ZhengKarl#gmail.com
+#       COMPANY: Alibaba
+#       CREATED: 2014年04月10日 17时24分54秒 CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+function adblistpackages()
+{
+	adb shell pm list packages -f "$@"
+}
