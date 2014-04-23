0eb24d215c1660e3f90ee40b1e05cc97352097bb
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-04-23 21:14:04 +0800

    modified:   newInstall/svn/process.sh
    Signed-off-by: kangliang.zkl <kangliang.zkl@alibaba-inc.com>

--- newInstall/svn/process.sh
+++ newInstall/svn/process.sh
@@ -1,4 +1,20 @@
-#!/bin/bash - 
+#!/bin/bash -
+
+# 1. svnadmin create xxx_repo
+
+# 2. conf repo:
+#add user and passwd in:
+#conf/authz
+#conf/passwd
+
+#vi conf/svnserve.conf
+#add:
+#anon-access = none
+#auth-access = write
+#password-db = passwd
+#authz-db = authz
+
+#3. make it boot with system
 
 #http://blog.chinaunix.net/uid-29158166-id-3983119.html
 sudo chmod a+x /etc/init.d/svnd.sh
