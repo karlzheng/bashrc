3e7b2a41c5e3aec4e040120203a5e338e2a919d2
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-06-17 18:27:13 +0800

    modified: 10execInNew.sh: dont install CapsLock key indicator for Thinkpad
    Signed-off-by: kangliang.zkl <kangliang.zkl@alibaba-inc.com>

--- newInstall/10execInNew.sh
+++ newInstall/10execInNew.sh
@@ -5,7 +5,8 @@ sudo bash -c "cat /tmp/tmp.txt >> /etc/sudoers"
 
 sudo apt-get install ssh -y
 
-sudo add-apt-repository ppa:tsbarnes/indicator-keylock
-sudo apt-get update
-sudo apt-get install indicator-keylock -y
-setsid indicator-keylock 
+## for Thinkpad CapsLock key indicator.
+#sudo add-apt-repository ppa:tsbarnes/indicator-keylock
+#sudo apt-get update
+#sudo apt-get install indicator-keylock -y
+#setsid indicator-keylock 
