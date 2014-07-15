0566a59c65c7d1d98869301bb233779de827bc44
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-07-15 20:21:45 +0800

    modified:   script/formatCrypt.sh
    Signed-off-by: kangliang.zkl <kangliang.zkl@alibaba-inc.com>

--- script/formatCrypt.sh
+++ script/formatCrypt.sh
@@ -19,45 +19,49 @@ function is_loopi_mounted()
 
 function format_crypt_disk()
 {
-    for img in $(dir *.img);
-    do
-	imgdir=${img%%.*}
-	cnt=0
-
-	while [ $cnt -lt 8 ]
+	for img in $(dir *.img);
 	do
-	    is_mounted=0
-	    is_loopi_mounted $cnt
-	    if [ $is_mounted == 0 ];then
-		break;
-	    fi
-	    ((cnt++))
-	done
+		imgdir=${img%%.*}
+		cnt=0
 
-	if [ $cnt -lt 8 ];then
-	    echo loop_dev="/dev/loop$cnt"
-	    loop_dev="/dev/loop$cnt"
-	    echo "sudo losetup $loop_dev $img"
-	    sudo losetup $loop_dev $img
-	    sudo cryptsetup create $imgdir $loop_dev
-            sudo mkfs.ext4 /dev/mapper/$imgdir
-	    sleep 2
-	    if [ ! -d $imgdir ];then
-		mkdir $imgdir
-	    fi
-	    sudo mount /dev/mapper/$imgdir $imgdir
-	    sudo chown $(whoami).$(whoami) $imgdir -R
-	    sleep 2
-	    sudo umount $imgdir
-	    sudo cryptsetup remove /dev/mapper/$imgdir
-	    if [ "x$dev_name" != "x" ];then
-		sudo losetup -d $dev_name
-	    fi
-	else
-	    echo "Has no loop dev to mount!!"
-	    exit 1
-	fi
-    done
+		while [ $cnt -lt 8 ]
+		do
+			is_mounted=0
+			is_loopi_mounted $cnt
+			if [ $is_mounted == 0 ];then
+				break;
+			fi
+			((cnt++))
+		done
+
+		if [ $cnt -lt 8 ];then
+			echo loop_dev="/dev/loop$cnt"
+			loop_dev="/dev/loop$cnt"
+			echo "sudo losetup $loop_dev $img"
+			sudo losetup $loop_dev $img
+			sudo cryptsetup create $imgdir $loop_dev
+			sudo mkfs.ext4 /dev/mapper/$imgdir
+			sleep 2
+			if [ ! -d $imgdir ];then
+				mkdir $imgdir
+			fi
+			sudo mount /dev/mapper/$imgdir $imgdir
+			sudo chown $(whoami).$(whoami) $imgdir -R
+			sleep 2
+			sudo umount $imgdir
+			sudo cryptsetup remove /dev/mapper/$imgdir
+			if [ "x$dev_name" != "x" ];then
+				sudo losetup -d $dev_name
+			fi
+			ls -d $imgdir/* 1>/dev/null 2>&1
+			if [ $? != 0 ];then
+				rmdir $imgdir
+			fi
+		else
+			echo "Has no loop dev to mount!!"
+			exit 1
+		fi
+	done
 }
 
 format_crypt_disk
