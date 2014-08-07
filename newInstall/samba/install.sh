447d33709e106c8325520b855d40226e98868e10
kangliang.zkl <kangliang.zkl@alibaba-inc.com>
2014-08-07 21:24:44 +0800

    modified:   samba/install.sh
    Signed-off-by: kangliang.zkl <kangliang.zkl@alibaba-inc.com>

--- newInstall/samba/install.sh
+++ newInstall/samba/install.sh
@@ -1,15 +1,23 @@
-# samba
+#!/bin/bash - 
+
+if [ -z "${SMBPREDIR}" ];then
+	echo "pls set SMBPREDIR var"
+	exit;
+fi
+
 sudo smbpasswd -a $(whoami)
 
-sudo mkdir -p /4t/samba/personal
-sudo chmod 777 /4t/samba/personal
-sudo chown $(whoami).$(whoami) /4t/samba/personal
+sudo mkdir -p ${SMBPREDIR}/personal
+sudo chmod 777 ${SMBPREDIR}/personal
+sudo chown $(whoami).$(whoami) ${SMBPREDIR}/personal
 
-sudo mkdir -p /4t/samba/share
-sudo chmod 777 /4t/samba/share
-sudo chown $(whoami).$(whoami) /4t/samba/share
+sudo mkdir -p ${SMBPREDIR}/share
+sudo chmod 777 ${SMBPREDIR}/share
+sudo chown $(whoami).$(whoami) ${SMBPREDIR}/share
 
 sed -e "s#karlzheng#$(whoami)#g" smb.conf > /tmp/tmp.conf
+
+sed -i -e "s#\${SMBPREDIR}#${SMBPREDIR}#g" /tmp/smb.conf
 
 sudo bash -c "cp /etc/samba/smb.conf /etc/samba/smb.conf.org"
 
