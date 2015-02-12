36a6f1cf43f97e7bd71bc6314b5f48b7a46a8c42
zhengkl <zhengkl@meizu.com>
2015-02-12 22:34:42 +0800

    new file:   script/ssh/ssh_sock_proxy.sh
    Signed-off-by: zhengkl <zhengkl@meizu.com>

---
+++ script/ssh/ssh_sock_proxy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  ssh_sock_proxy.sh
+# 
+#         USAGE:  ./ssh_sock_proxy.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: Karl Zheng (), ZhengKarl#gmail.com
+#       COMPANY: Meizu
+#       CREATED: 2015年02月12日 21时52分29秒 CST
+#      REVISION:  ---
+#===============================================================================
+
+#http://straightedgelinux.com/blog/howto/socks.html
+ssh -Nn -D 1080 klaatu@home.linuxserver.com 
