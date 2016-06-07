d296ab45205a6cb4391bb4f897c52f70f58473f2
KarlZheng <zhengkl@meizu.com>
2016-06-07 21:08:26 +0800

    modified:   script/ragain.sh
    Signed-off-by: KarlZheng <zhengkl@meizu.com>

--- script/ragain.sh
+++ script/ragain.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo "======start repo sync======"
-echo repo sync -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
-repo sync -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
+echo repo sync -c -d -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
+repo sync -c -d -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
 while [ $? = 1 ]; do
         echo "======sync failed, re-sync again======"
         sleep 3
         #repo sync
-	echo repo sync -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
-	#repo sync -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)*2}')
-	repo sync -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
+    echo repo sync -c -d -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
+    #repo sync -c -d -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)*2}')
+    repo sync -c -d -j$(/bin/grep processor /proc/cpuinfo | /usr/bin/awk '{field=$NF};END{print(field+1)}')
 done
-
