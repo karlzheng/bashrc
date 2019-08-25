45f6bfa8d2ce066722f60059ba22da86c15e436b
KarlZheng <KarlZheng@linkcm.com>
2019-08-25 20:06:09 +0800

    add function privoxy_proxy
    Signed-off-by: KarlZheng <KarlZheng@linkcm.com>

--- karlzheng_config/mybashrc.sh
+++ karlzheng_config/mybashrc.sh
@@ -1273,6 +1273,16 @@ function pf()
 	ps -ef "$@"
 }
 
+function privoxy_proxy()
+{
+	if [ OS=="Mac" ];then
+		http_proxy="http://127.0.0.1:8118"
+		export http_proxy
+		https_proxy="http://127.0.0.1:8118"
+		export https_proxy
+	fi
+}
+
 function pu()
 {
 	if [ $# -ge 1 ];then
