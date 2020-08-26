9b9c7842a51599226939f9b0f0e2e951db215e2e
Karl.Zheng <Karl.Zheng@anker.com>
2020-08-26 17:08:43 +0800

    [Feat] modify: jget.sh seplog.sh
    Signed-off-by: Karl.Zheng <Karl.Zheng@anker.com>

--- script/jget.sh
+++ script/jget.sh
@@ -2,7 +2,12 @@
 
 function get_jira_attch_files()
 {
-	local url=$(ds)
+	if [ $# -ge 1 ];then
+		local url=$1
+		shift
+	else
+		local url=$(ds)
+	fi
 	local fn=$(echo ${url##*/})
 	local jn=$(echo ${url%/*})
 	jira_number=$(echo ${jn##*/})
@@ -17,7 +22,8 @@ function get_jira_attch_files()
 	url="${prefix}//${suffix}"
 	#echo "${url} ${fn}"
 
-	curl "${url}" -XGET -H "Cookie: jira.editor.user.mode=wysiwyg; JSESSIONID=${JSESSIONID}; atlassian.xsrf.token=${JIRA_TOKEN}; mo.jira-oauth.baseurl=https://jira.anker-in.com; mo.jira-oauth.logoutcookie=e6ca6f41-b9b3-4e31-ac14-2da321db6a63; _ga=GA1.2.1710698958.1569467211" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Host: jira.anker-in.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15' -H 'Accept-Language: zh-cn' -H 'Accept-Encoding: ""' -H 'Connection: keep-alive' -o "${fn}" "$@"
+	echo curl "${url}" -XGET -H "Cookie: jira.editor.user.mode=wysiwyg; JSESSIONID=${JSESSIONID}; atlassian.xsrf.token=${atlassianxsrftoken}; mo.jira-oauth.baseurl=https://jira.anker-in.com; mo.jira-oauth.logoutcookie=e6ca6f41-b9b3-4e31-ac14-2da321db6a63; _ga=GA1.2.1710698958.1569467211" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Host: jira.anker-in.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15' -H 'Accept-Language: zh-cn' -H 'Accept-Encoding: ""' -H 'Connection: keep-alive' -o "${fn}" "$@"
+	curl "${url}" -XGET -H "Cookie: jira.editor.user.mode=wysiwyg; JSESSIONID=${JSESSIONID}; atlassian.xsrf.token=${atlassianxsrftoken}; mo.jira-oauth.baseurl=https://jira.anker-in.com; mo.jira-oauth.logoutcookie=e6ca6f41-b9b3-4e31-ac14-2da321db6a63; _ga=GA1.2.1710698958.1569467211" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Host: jira.anker-in.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15' -H 'Accept-Language: zh-cn' -H 'Accept-Encoding: ""' -H 'Connection: keep-alive' -o "${fn}" "$@"
 }
 
-get_jira_attch_files "$@"
+get_jira_attch_files "$*"
--- script/seplog.sh
+++ script/seplog.sh
@@ -5,7 +5,8 @@ function seplog()
 	local fn=$(lf)
 	read -p "seplog ${fn} y|n ?" c
 	if [ "x${c}" != "x" ];then
-		fn=$(find . -regex '.*\.\(log\|txt\)')
+		#fn=$(find . -regex '.*\.\(log\|txt\)')
+		fn=$(find . -maxdepth 1 -regex '.*\.\(log\|txt\)' | xargs ls -lt | head -n 1 | awk '{print $NF}')
 		read -p "seplog ${fn} y|n ?" c
 		if [ "x${c}" != "x" ];then
 			return;
@@ -16,6 +17,8 @@ function seplog()
 		return;
 	fi
 	local log_dir=$(echo ${fn%\.*})
+	local filter_file="${log_dir}/${fn}.filter.txt"
+
 	mkdir -p ${log_dir}
 	while read l; do
 		echo ${l}
@@ -24,17 +27,22 @@ function seplog()
 		grep -q -Hn "${l}" "${fn}"
 		if [ $? == 0 ];then
 			grep -Hn "${l}" "${fn}" | tee ${log_dir}/${kfn}
+			cat ${log_dir}/${kfn} >> ${filter_file}
 		fi
 	done < ~/log.keyword.txt
+	sort ${filter_file} -o ${filter_file}
+	/bin/cp ${fn} ${log_dir}/${fn}
+	#vi ${filter_file} ${log_dir}/${fn}
+	vim -c "BESHighlight" ${filter_file} ${log_dir}/${fn}
 
-	read -p "cd ${log_dir} y|n ?" c
-	if [ "x${c}" == "xy" -o "x${c}" == "xY" -o "x${c}" == "x" ];then
-		cd ${log_dir}
-		read -p "vi * y|n ?" c
-		if [ "x${c}" == "xy" -o "x${c}" == "xY" -o "x${c}" == "x" ];then
-			vi *
-		fi
-	fi
+	#read -p "cd ${log_dir} y|n ?" c
+	#if [ "x${c}" == "xy" -o "x${c}" == "xY" -o "x${c}" == "x" ];then
+		#cd ${log_dir}
+		#read -p "vi * y|n ?" c
+		#if [ "x${c}" == "xy" -o "x${c}" == "xY" -o "x${c}" == "x" ];then
+			#vi *
+		#fi
+	#fi
 	#find . -name "*" -type f -size 0c | xargs -n 1 rm -f
 }
 
