f423c34303ec193808322d730faa25f052deeded
Karl.Zheng <Karl.Zheng@anker.com>
2020-08-27 15:12:32 +0800

    modified:   script/jget.sh
    Signed-off-by: Karl.Zheng <Karl.Zheng@anker.com>

--- script/jget.sh
+++ script/jget.sh
@@ -26,4 +26,22 @@ function get_jira_attch_files()
 	curl "${url}" -XGET -H "Cookie: jira.editor.user.mode=wysiwyg; JSESSIONID=${JSESSIONID}; atlassian.xsrf.token=${atlassianxsrftoken}; mo.jira-oauth.baseurl=https://jira.anker-in.com; mo.jira-oauth.logoutcookie=e6ca6f41-b9b3-4e31-ac14-2da321db6a63; _ga=GA1.2.1710698958.1569467211" -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Host: jira.anker-in.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15' -H 'Accept-Language: zh-cn' -H 'Accept-Encoding: ""' -H 'Connection: keep-alive' -o "${fn}" "$@"
 }
 
-get_jira_attch_files "$*"
+url=""
+while [ $# -gt 0 ];
+do
+    case $1 in
+	    -u)
+            shift
+            url="$1"
+            ;;
+        *)
+            ;;
+    esac
+    shift
+done
+
+if [ "x${url}" != "x" ];then
+	get_jira_attch_files "${url}"
+else
+	get_jira_attch_files
+fi
