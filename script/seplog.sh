4f823c0a32c4494f6798365c1dbec92384c4b492
Karl.Zheng <Karl.Zheng@anker.com>
2021-01-13 13:59:41 +0800

    modified:   script/seplog.sh
    Signed-off-by: Karl.Zheng <Karl.Zheng@anker.com>

--- script/seplog.sh
+++ script/seplog.sh
@@ -4,6 +4,7 @@ function seplog()
 {
 	local fn=$(lf)
 	read -p "seplog ${fn} y|n ?" c
+
 	if [ "x${c}" != "x" ];then
 		#fn=$(find . -regex '.*\.\(log\|txt\)')
 		fn=$(find . -maxdepth 1 -regex '.*\.\(log\|txt\)' | xargs ls -lt | head -n 1 | awk '{print $NF}')
@@ -12,10 +13,12 @@ function seplog()
 			return;
 		fi
 	fi
+
 	if [ "x${fn}" == "x" ];then
 		echo "Not Found *.log | *.txt"
 		return;
 	fi
+
 	local log_dir=$(echo ${fn%\.*})
 	local filter_file="${log_dir}/${fn}.filter.txt"
 
@@ -25,17 +28,19 @@ function seplog()
 	fi
 
 	mkdir -p ${log_dir}
+
 	while read l; do
 		echo ${l}
 		local kfn=$(echo "${l}" | sed s/[[:space:]]//g)
 		echo grep "${l}" "${fn}"
 		grep -q -Hn "${l}" "${fn}"
 		if [ $? == 0 ];then
-			grep -Hn "${l}" "${fn}" | tee ${log_dir}/${kfn}
-			cat ${log_dir}/${kfn} >> ${filter_file}
+			grep -Hn "${l}" "${fn}" >> ${filter_file}
 		fi
 	done < ~/log.keyword.txt
+
 	sort -n -k 2 -t : ${filter_file} -o ${filter_file}
+
 	/bin/cp ${fn} ${log_dir}/${fn}
 	#vi ${filter_file} ${log_dir}/${fn}
 	vim -c "BESHighlight" ${filter_file} ${log_dir}/${fn}
