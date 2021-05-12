882b2db7eb60b3a2ccaffb8fbd3317663c9dc2d4
Karl.Zheng <Karl.Zheng@anker.com>
2021-05-12 15:00:27 +0800

    [Feat] modify: script/gencscopetags.sh
    Signed-off-by: Karl.Zheng <Karl.Zheng@anker.com>

--- script/gencscopetags.sh
+++ script/gencscopetags.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
-echo "$0 : $(pwd) : parameters:$@"
-if [ -n "$1" ]; then
-    echo "find files in dir:$1"
-    #find $1 -iname "Makefile" -o -iname "*.h" -o -iname "*.hpp" -o -iname "*.c" -o -iname "*.cc" -o -iname "*.cpp" -o -iname "*.java" -o -iname "*.s" > cscope.files
-	find "$1" -type f -a -regextype posix-extended -regex ".*(\.((c$)|(h$)|(cc$)|(hh$)|(cpp$)|(cxx$)|(hpp$)|(java$)|(s$))|Makefile|MAKEFILE|Kconfig)" > cscope.files
-else
-    echo "find files in current dir"
-	if [ "$(uname -s)" == "Darwin" ];then
-		: > cscope.files
-		file_suffix_list=(c h hh hpp cc cpp java s)
-		for fs in ${file_suffix_list[@]};do
-			find "." -type f -a -regex ".*\.${fs}$"  >> cscope.files
-		done
-		file_list=(Makefile Kconfig)
-		for fs in ${file_list[@]};do
-			find "." -iname ${fs} >> cscope.files
-		done
-	else
-		find "." -type f -a -regextype posix-extended -regex ".*(\.((c$)|(h$)|(cc$)|(hh$)|(cpp$)|(cxx$)|(hpp$)|(java$)|(s$))|Makefile|MAKEFILE|Kconfig)" > cscope.files
-	fi
-fi
-echo "end find"
-awk '{print "\""$0"\""}' cscope.files > cscope.files.tmp
-mv cscope.files.tmp cscope.files
-echo "begin cs"
-date
-#cscope -bkqU -i cscope.files
-if [ -d arch/arm/configs ];then
-    cscope -bkq -i cscope.files
-else
-    cscope -bq -i cscope.files
-fi
-echo "end cs"
+function gencscopetags.sh.func()
+{
+	echo "$0 : $(pwd) : parameters:$@"
 
+	if [ -n "$1" ]; then
+		local file_dir="$1"
+	else
+		local file_dir="."
+	fi
+
+	echo "find files in dir: ${file_dir}"
+	local regex_posix="-regextype posix-extended"
+	if [ "$(uname -s)" == "Darwin" ];then
+		regex_posix=""
+	fi
+
+	: > cscope.files
+	local file_suffix_list=(c h hh hpp cc cpp cxx hpp java s)
+	for ext in ${file_suffix_list[@]};do
+		find ${file_dir} -type f -a ${regex_posix} -regex ".*\.${ext}$"  >> cscope.files
+	done
+
+	local file_list=(Makefile MAKEFILE Kconfig)
+	for fn in ${file_list[@]};do
+		find ${file_dir} -iname ${fn} >> cscope.files
+	done
+	echo "end find"
+
+	awk '{print "\""$0"\""}' cscope.files > cscope.files.tmp
+	mv cscope.files.tmp cscope.files
+
+	echo "$(date) begin cscope"
+	#cscope -bkqU -i cscope.files
+	if [ -d arch/arm/configs ];then
+		cscope -bkq -i cscope.files
+	else
+		cscope -bq -i cscope.files
+	fi
+	echo "$(date) begin cscope"
+}
+
+gencscopetags.sh.func "$@"
